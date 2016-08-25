76e5d01f42c465e4efd53f07c1dcc4ce6c8f38e1
Mattressburn <chevyboxer@gmail.com>
2016-08-25 15:06:36 -0400

    first push

---
+++ app.js
@@ -0,0 +1,59 @@
+var question1 = {
+   text: "What was the first Nintendo game Mario appeared in ",
+   choices: ["Mario Brothers","Donkey Kong","Metroid", "Zelda"],
+   correctAnswer : 1
+}
+
+var question2 = {
+  text: "What is the name of Mario's Brother"
+  choices: ["Wario","Waluigi","Luigi","Toad" ]
+  correctAnswer : 2
+}
+
+var question3 = {
+  text: "What was the first multi-game Console"
+  choices: ["Atari 5300","NES","Atari 2600","Gameboy" ]
+  correctAnswer : 2
+}
+
+var question4 = {
+  text: "What was the first Pokemon Game"
+  choices: ["Yellow","Red","Gold","Blue" ]
+  correctAnswer : 3
+}
+
+var question5 = {
+  text: "When was Nintendo founded"
+  choices: ["1980","1975","1973","1889" ]
+  correctAnswer : 3
+}
+
+var question6 = {
+  text: "In what game does your character try and cross the road while dodging cars"
+  choices: ["Duck Hunt","Cross the Road","Chicken","Frogger" ]
+  correctAnswer : 2
+}
+
+var question7 = {
+  text: "What was the sequel to Tron"
+  choices: ["Tron Strikes Back","Return of the Tron","Discs of Tron","Back to the Tron" ]
+  correctAnswer : 2
+}
+
+var question8 = {
+  text: "What was one of the names of the Ghosts in Pac-Man"
+  choices: ["Clyde","Boo","Casper","Pinkguy" ]
+  correctAnswer : 0
+}
+
+var question9 = {
+  text: "Who is the Hero of the Zelda"
+  choices: ["Zelda","Mario","Link","Samus" ]
+  correctAnswer : 2
+}
+
+var question10 = {
+  text: "In what country was Tetris Developed"
+  choices: ["Japan","China","'Murica","Russia" ]
+  correctAnswer : 3
+}
